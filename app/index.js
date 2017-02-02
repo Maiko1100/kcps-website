6ea98c281340fbf0a996cf58a91df7847b05eb9f
maiko kuipers <maiko@061.222.028.145.hva.nl>
2017-02-02 17:09:48 +0100

    Package joon veranderd

--- app/index.js
+++ app/index.js
@@ -3,21 +3,19 @@ import ReactDOM from "react-dom"
 import { Provider } from "react-redux"
 import Login from "./components/Login"
 import store from "./store"
-import { applyRouterMiddleware ,Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { Router, Route, browserHistory } from 'react-router'
 import { syncHistoryWithStore,} from 'react-router-redux'
 import App from './components/App'
 import { UserIsAuthenticated, UserIsNotAuthenticated } from './utils/authWrappers.js'
 const baseHistory = browserHistory
 const history = syncHistoryWithStore(baseHistory, store)
-import { useScroll } from 'react-router-scroll';
 
 
 ReactDOM.render(
 
     <Provider store={store}>
         {/* hashHistory ---> browserHistory when the site is on a server for clean urls*/}
-        <Router history={history} render={applyRouterMiddleware(useScroll())}>
-
+        <Router history={history}>
             <Route path="/" component={App}>
                 <Route path="/Login" component={UserIsNotAuthenticated(Login)} />
             </Route>
